f7504c7df521fa94ea030bfa440c3cea856efb1b
oGuilhermeOliveira <guilhermeolsnto@gmail.com>
2024-06-08 20:58:41 -0300

    inicio

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import Teste from './Teste'
 import EstiloGlobal from './styles'
+import Sobre from './containers/Sidebar/About'
+import Sidebar from './containers/Sidebar'
 
 function App() {
   return (
     <>
-      <Teste />
       <EstiloGlobal />
+      <Sidebar />
+      <main>
+        <Sobre />
+        <section>Projetos</section>
+      </main>
     </>
   )
 }
---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Title from '../../components/title'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/oGuilhermeOliveira.png"></img>
+    <Title>Guilherme Oliveira</Title>
+  </aside>
+)
+
+export default Sidebar
