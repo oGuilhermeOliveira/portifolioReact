071546bef1d076ccfa0a184d43654ec626c6903c
Guilherme Oliveira dos Santos <guilhermeolsnto@gmail.com>
2024-06-14 00:39:22 -0300

    finalizado

--- src/components/title/styles.ts
+++ src/components/title/styles.ts
@@ -6,5 +6,5 @@ export const Titulo = styled.h3<Props>`
   color: ${(props) => props.theme.corPrincipal};
   font-size: ${(props) => (props.fontSize ? props.fontSize + 'px' : '14px')};
   font-weight: bold;
-  margin-bottom: 16px;
+  margin-bottom: 15px;
 `
