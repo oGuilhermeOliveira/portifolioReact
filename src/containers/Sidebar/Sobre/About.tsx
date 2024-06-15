84c75fa9aea555d49289fa8150a9c7ea5971c698
Guilherme Oliveira dos Santos <guilhermeolsnto@gmail.com>
2024-06-15 09:37:15 -0300

    finalizado

--- src/containers/Sidebar/Sobre/About.tsx
+++ src/containers/Sidebar/Sobre/About.tsx
@@ -6,10 +6,12 @@ const Sobre = () => (
   <section>
     <Title fontSize={16}>Sobre mim</Title>
     <Paragrafo tipo="secundario">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime soluta,
-      quos repellat nam laboriosam explicabo, accusantium error nisi iure
-      consectetur animi accusamus consequatur omnis, ducimus sed modi deserunt
-      dignissimos voluptates.
+      I am Professional with a career in evolution in the area of Full Stack
+      Java Development. I have as a principle the valuation of overcoming
+      obstacles as a source of motivation, to seek the knowledge and resources
+      necessary to deal with challenges. I enjoy exercising my skills and
+      delving deeper into my area, developing increasing skills in these areas,
+      so that being seen as a reference becomes a motivating factor.
     </Paragrafo>
     <GithubSecao>
       <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=oGuilhermeOliveira&layout=compact&langs_count=7&theme=aura" />
